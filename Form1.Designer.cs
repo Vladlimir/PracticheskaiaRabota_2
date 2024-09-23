64e231e843978bfa037a11b30f5c2f827eb3a23a
Мичко_ВВ <_@H13>
2024-09-23 12:42:40 +0800

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Практическая_работа_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.textBoxMaterialName = new System.Windows.Forms.TextBox();
+            this.textBoxSort = new System.Windows.Forms.TextBox();
+            this.textBoxProfile = new System.Windows.Forms.TextBox();
+            this.textBoxSize = new System.Windows.Forms.TextBox();
+            this.textBoxUnitOfMeasurement = new System.Windows.Forms.TextBox();
+            this.textBoxNomenclatureNumber = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.textBoxReserveNorm = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(271, 42);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Поиск";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(75, 42);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(165, 20);
+            this.textBoxSearch.TabIndex = 1;
+            this.textBoxSearch.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxMaterialName
+            // 
+            this.textBoxMaterialName.Location = new System.Drawing.Point(105, 97);
+            this.textBoxMaterialName.Name = "textBoxMaterialName";
+            this.textBoxMaterialName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMaterialName.TabIndex = 2;
+            this.textBoxMaterialName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxSort
+            // 
+            this.textBoxSort.Location = new System.Drawing.Point(105, 135);
+            this.textBoxSort.Name = "textBoxSort";
+            this.textBoxSort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSort.TabIndex = 3;
+            this.textBoxSort.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxProfile
+            // 
+            this.textBoxProfile.Location = new System.Drawing.Point(105, 170);
+            this.textBoxProfile.Name = "textBoxProfile";
+            this.textBoxProfile.Size = new System.Drawing.Size(100, 20);
+            this.textBoxProfile.TabIndex = 4;
+            this.textBoxProfile.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxSize
+            // 
+            this.textBoxSize.Location = new System.Drawing.Point(105, 209);
+            this.textBoxSize.Name = "textBoxSize";
+            this.textBoxSize.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSize.TabIndex = 5;
+            this.textBoxSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxUnitOfMeasurement
+            // 
+            this.textBoxUnitOfMeasurement.Location = new System.Drawing.Point(105, 253);
+            this.textBoxUnitOfMeasurement.Name = "textBoxUnitOfMeasurement";
+            this.textBoxUnitOfMeasurement.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUnitOfMeasurement.TabIndex = 6;
+            this.textBoxUnitOfMeasurement.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxNomenclatureNumber
+            // 
+            this.textBoxNomenclatureNumber.Location = new System.Drawing.Point(105, 292);
+            this.textBoxNomenclatureNumber.Name = "textBoxNomenclatureNumber";
+            this.textBoxNomenclatureNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNomenclatureNumber.TabIndex = 7;
+            this.textBoxNomenclatureNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(105, 333);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrice.TabIndex = 8;
+            this.textBoxPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxReserveNorm
+            // 
+            this.textBoxReserveNorm.Location = new System.Drawing.Point(105, 377);
+            this.textBoxReserveNorm.Name = "textBoxReserveNorm";
+            this.textBoxReserveNorm.Size = new System.Drawing.Size(100, 20);
+            this.textBoxReserveNorm.TabIndex = 9;
+            this.textBoxReserveNorm.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxReserveNorm);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxNomenclatureNumber);
+            this.Controls.Add(this.textBoxUnitOfMeasurement);
+            this.Controls.Add(this.textBoxSize);
+            this.Controls.Add(this.textBoxProfile);
+            this.Controls.Add(this.textBoxSort);
+            this.Controls.Add(this.textBoxMaterialName);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.buttonSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.TextBox textBoxMaterialName;
+        private System.Windows.Forms.TextBox textBoxSort;
+        private System.Windows.Forms.TextBox textBoxProfile;
+        private System.Windows.Forms.TextBox textBoxSize;
+        private System.Windows.Forms.TextBox textBoxUnitOfMeasurement;
+        private System.Windows.Forms.TextBox textBoxNomenclatureNumber;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.TextBox textBoxReserveNorm;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Практическая_работа_2
+{
+    public partial class Form1 : Form
+    {
+        private Material[] materials;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMaterials();
+        }
+
+        private void InitializeMaterials()
+        {
+            materials = new Material[]
+            {
+                new Material { Name = "Материал1", Sort = "Сорт1", Profile = "Профиль1", Size = "Размер1", UnitOfMeasurement = "Единица1", NomenclatureNumber = "Номенклатурный номер1", Price = 10.5, ReserveNorm = 5 },
+                new Material { Name = "Материал2", Sort = "Сорт2", Profile = "Профиль2", Size = "Размер2", UnitOfMeasurement = "Единица2", NomenclatureNumber = "Номенклатурный номер2", Price = 20.5, ReserveNorm = 10 },
+                // Add more materials here...
+            };
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string materialName = textBoxSearch.Text;
+            Material foundMaterial = Array.Find(materials, m => m.Name == materialName);
+            if (foundMaterial != null)
+            {
+                textBoxMaterialName.Text = foundMaterial.Name;
+                textBoxSort.Text = foundMaterial.Sort;
+                textBoxProfile.Text = foundMaterial.Profile;
+                textBoxSize.Text = foundMaterial.Size;
+                textBoxUnitOfMeasurement.Text = foundMaterial.UnitOfMeasurement;
+                textBoxNomenclatureNumber.Text = foundMaterial.NomenclatureNumber;
+                textBoxPrice.Text = foundMaterial.Price.ToString();
+                textBoxReserveNorm.Text = foundMaterial.ReserveNorm.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Материал не найден");
+            }
+        }
+    }
+
+    public class Material
+    {
+        public string Name { get; set; }
+        public string Sort { get; set; }
+        public string Profile { get; set; }
+        public string Size { get; set; }
+        public string UnitOfMeasurement { get; set; }
+        public string NomenclatureNumber { get; set; }
+        public double Price { get; set; }
+        public int ReserveNorm { get; set; }
+    }
+}
